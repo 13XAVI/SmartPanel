e075c04f07a099fe3bb3cdefc2315e8c7e6bc850
13XAVI <tresorxavier16@gmail.com>
2023-05-16 20:49:41 +0200

    moved Dir

--- FrontEnd/SmartPanel/src/main/java/SmartPanel/panel/Reservation/SmartPanel/Repositories/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package SmartPanel.panel.Reservation.SmartPanel.Repositories;
-
-import SmartPanel.panel.Reservation.SmartPanel.Model.PanelRoles;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-public interface RoleRepository extends JpaRepository<PanelRoles,Integer> {
-
-}
